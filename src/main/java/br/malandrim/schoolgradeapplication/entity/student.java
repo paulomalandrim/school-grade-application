91d9b52539870cbe067de1b9dd600766e63e5b97
paulomalandrim <paulo.malandrim@gmail.com>
2024-02-11 11:31:37 -0300

    .

--- src/main/java/br/malandrim/schoolgradeapplication/entity/student.java
+++ src/main/java/br/malandrim/schoolgradeapplication/entity/student.java
@@ -1,4 +1,9 @@
 package br.malandrim.schoolgradeapplication.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
 public class student {
+    @Column private String name;
 }
