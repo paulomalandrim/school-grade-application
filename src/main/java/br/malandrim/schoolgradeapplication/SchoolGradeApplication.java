3f7985c5281366d4d13c7b9665c291c2247c4986
paulomalandrim <paulo.malandrim@gmail.com>
2024-02-11 11:14:14 -0300

    .

---
+++ src/main/java/br/malandrim/schoolgradeapplication/SchoolGradeApplication.java
@@ -0,0 +1,13 @@
+package br.malandrim.schoolgradeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/malandrim/schoolgradeapplication/SchoolGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.malandrim.schoolgradeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
